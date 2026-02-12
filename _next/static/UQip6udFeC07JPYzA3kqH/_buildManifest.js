1b090fd9b68d17e7c269cf23b9c488a083d6a463
brightwavebot <cyberbitlabs@gmail.com>
2026-02-12 09:23:59 +0000

    Deploy build from af97332

---
+++ _next/static/UQip6udFeC07JPYzA3kqH/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/jbguptahospital//_next/:path+",
+        "destination": "/jbguptahospital/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
